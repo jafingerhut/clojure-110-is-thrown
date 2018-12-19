2b28d6cfa6361373167767d351a29b1ddff25866
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-12-19 10:22:01 -0800

    Update expected error message to accomodate changes in Clojure 1.10

--- test/clojure_110_is_thrown/core_test.clj
+++ test/clojure_110_is_thrown/core_test.clj
@@ -2,10 +2,20 @@
   (:require [clojure.test :refer :all]
             [clojure-110-is-thrown.core :refer :all]))
 
+(defn min-clojure-version [min-version-vector]
+  (>= (compare ((juxt :major :minor) *clojure-version*)
+               min-version-vector)
+      0))
+
 (deftest test1
+  (println "*clojure-version* is: " *clojure-version*)
   (let [vec1 [:a :b :c]]
     (is (thrown? IndexOutOfBoundsException
                  (vec1 3)))
-    (is (thrown-with-msg? clojure.lang.ArityException
-                          #"Wrong number of args \(2\) passed to: PersistentVector"
-                          (vec1 3 :not-found)))))
+    (let [expected-error-msg-regex
+          (if (min-clojure-version [1 10])
+            #"Wrong number of args \(2\) passed to: clojure.lang.PersistentVector"
+            #"Wrong number of args \(2\) passed to: PersistentVector")]
+      (is (thrown-with-msg? clojure.lang.ArityException
+                            expected-error-msg-regex
+                            (vec1 3 :not-found))))))
